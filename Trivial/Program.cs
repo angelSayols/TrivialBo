3262384d5dd266054b01481bc7618ffe5274fa07
angel <a2308744@institutmontilivi.cat>
2024-02-08 15:51:33 +0100

    before adding hard code

--- Trivial/Program.cs
+++ Trivial/Program.cs
@@ -19,14 +19,13 @@ namespace Trivial
         
             do
             {
-
                 ShowOptions();
                 tecla = Console.ReadKey();
                 Console.Clear();
                 switch (tecla.Key)
                 {
                     case ConsoleKey.D1:
-                        //historia 1
+                        //historia
                         break;
                     case ConsoleKey.D2:
                         //sport 2
@@ -52,23 +51,35 @@ namespace Trivial
             } while (tecla.Key != ConsoleKey.D0);
         }
 
+        public static void ShowOptions()
+        {
+            Console.WriteLine("TRY ONE OF THIS THEMES: ");
+            Console.WriteLine("PRESS 1 FOR HISTORY");
+            Console.WriteLine("PRESS 2 FOR SPORTS");
+            Console.WriteLine("PRESS 3 FOR ART");
+            Console.WriteLine("PRESS 4 FOR GEOGRAPHY");
+            Console.WriteLine("PRESS 5 FOR ENTERTAINMENT");
+            Console.WriteLine("PRESS 6 FOR MATHS");
+            Console.WriteLine("PRESS 0 FOR LEAVING");
+        }
+
         private static void crearPreguntes(questionari[] h, questionari[] s, questionari[] a, questionari[] g, questionari[] e, questionari[] m)
         {
-            h[0] = { };
-            h[1] = { };
-            h[2] = { };
-            h[3] = { };
-            h[4] = { };
-            s[0] = { };
-            s[0] = { };
-            s[0] = { };
-            s[0] = { };
-            s[0] = { };
-            a[0] = { };
-            a[0] = { };
-            a[0] = { };
-            a[0] = { };
-            a[0] = { };
+            h[0] = new questionari("What year was born Franco?", "1) 5 August 2034","2) 4 December 1892","3) 2 November 1677"," 4) He was not born", "2");
+            h[1] = new questionari("When was the Eiffel finished?", "1) 31 March 1889", "2) 4 December 1892", "3) 2 November 1677", " 4)It is fake", "1");
+            h[2] = new questionari("What was taken the first photo?", "1) 1.826", "2) 1892", "3) 1677", "4) It was not taken", "1");
+            h[3] = new questionari("Who invented the Piruletas?", "1) Luis Jiménez Somacarrera", "2) Thomas Edison", "3) Nobody", "4) Fernando Alonso", "1");
+            h[4] = new questionari("How long was the WW2 ?", "1) 9", "2) 4", "3) 6", "4) 8999", "3");
+            s[0] = new questionari("Which sport is common in spain ?", "1) soccer", "2) baseball", "3) waterpolo", "4) skii", "1");
+            s[0] = new questionari("How long lasts a soccer game ?", "1) 100 min", "2) 45 min", "3) 90 min", "4) 6 hours", "3");
+            s[0] = new questionari("When was the first soccer mundial celebrated ?", "1) 1999", "2) 1931", "3) 3456", "4) 1930", "4");
+            s[0] = new questionari("Which country was the inventor of volleyball ?", "1) Spain", "2) USA", "3) France", "4) China", "2");
+            s[0] = new questionari("How is it called a point in football ?", "1) basket", "2) Pere", "3) goal", "4) touchdown", "4");
+            a[0] = new questionari("Who painted Guernica ?", "1) Picasso", "2) Lionel Messi", "3) Vivaldi", "4) Miguel Ángel", "1");
+            a[0] = new questionari("Which is the first name of Dalí ?", "1) Pere", "2) Johaness", "3) Salvador", "4) Mohamed", "3");
+            a[0] = new questionari("Who sculpted The Thinker ?", "1) Rodin", "2) Marc ", "3) Picasso", "4) Dalí", "1");
+            a[0] = new questionari("Which of this artists is Spanish ?", "1) Picasso", "2) Leonardo Da Vinci", "3) Tiziano", "4) Vladimir Putin", "1");
+            a[0] = new questionari("Who painted rare clocks ?", "1) Dalí", "2) Da Vinci", "3) Vladimir Putin", "4) Pedor Sánchez", "1");
             g[0] = { };
             g[0] = { };
             g[0] = { };
@@ -93,10 +104,7 @@ namespace Trivial
             Console.ReadKey();
         }
 
-        public static void ShowOptions()
-        {
-            Console.WriteLine("sdkfjsdlakf");
-        }
+        
 
         public static void tractamentTematica(List<string> preguntes)
         {
